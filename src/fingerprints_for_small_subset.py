5b5b96575d281479b262a1c1e7c02304bc5bc1ce
Diete Wouters <diete.wouters@student.kuleuven.be>
2026-03-24 16:05:49 +0100

    Small subset to fingerprints

---
+++ src/fingerprints_for_small_subset.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+from src.fingerprints import smiles_to_fingerprint
+
+
+def fingerprint_to_string(fp: np.ndarray) -> str:
+    return "".join(map(str, fp.astype(int)))
+
+
+def convert_csv_to_fingerprint_csv(input_path: str, output_path: str) -> None:
+    df = pd.read_csv(input_path)
+
+    if "smiles" not in df.columns or "h298" not in df.columns:
+        raise ValueError("CSV does not contain the correct columns. Required: 'smiles' and 'h298'.")
+
+    fingerprints = []
+    enthalpies = []
+
+    for _, row in df.iterrows():
+        smiles = row["smiles"]
+        h298 = row["h298"]
+
+        try:
+            fp = smiles_to_fingerprint(smiles)  
+            fp_str = fingerprint_to_string(fp)   #make string
+            fingerprints.append(fp_str)
+            enthalpies.append(h298)
+        except Exception as e:
+            print(f"Error '{smiles}': {e}")
+
+    out_df = pd.DataFrame({
+        "fingerprint": fingerprints,
+        "enthalpy": enthalpies
+    })
+
+    # save
+    out_df.to_csv(output_path, index=False)
+    print(f"New csv file saved: {output_path}")
+    
+input_file = "data/groupadditivity_h298/dataset/groupadditivity_0.004.csv"
+output_file = "data/groupadditivity_h298/dataset/fingerprints/groupadditivity_0.004_fingerprints.csv"
+
+convert_csv_to_fingerprint_csv(input_file, output_file)
